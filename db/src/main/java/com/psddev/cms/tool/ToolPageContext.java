3ff599e82d3b71e09d0a52dca7899263a755bc26
Hyoo Lim <hyoolim@gmail.com>
2016-12-16 10:31:54 -0500

    Stops depending on HUSL.

--- db/src/main/java/com/psddev/cms/tool/ToolPageContext.java
+++ db/src/main/java/com/psddev/cms/tool/ToolPageContext.java
@@ -1964,12 +1964,6 @@ public class ToolPageContext extends WebPageContext {
                 writeHtml("window.less.relativeUrls = true;");
             writeEnd();
 
-            writeStart("script", "type", "text/javascript", "src", cmsResource(scriptPrefix + "husl.js"));
-            writeEnd();
-
-            writeStart("script", "type", "text/javascript", "src", cmsResource(scriptPrefix + "husl-less.js"));
-            writeEnd();
-
             writeStart("script", "type", "text/javascript", "src", cmsResource(scriptPrefix + "less.js"));
             writeEnd();
         }
